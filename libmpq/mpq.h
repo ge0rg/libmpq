073064d0607d4320bbf7f2305f3c723a4b90e3bf
Maik Broemme <mbroemme@libmpq.org>
2008-04-21 11:00:19 +0000

    renamed **dest_mpq_archive to **mpq_archive to make definition same as function

--- libmpq/mpq.h
+++ libmpq/mpq.h
@@ -122,7 +122,7 @@ extern LIBMPQ_API int32_t libmpq__shutdown(void);
 extern LIBMPQ_API const char *libmpq__version(void);
 
 /* generic mpq archive information. */
-extern LIBMPQ_API int32_t libmpq__archive_open(mpq_archive_s **dest_mpq_archive, const char *mpq_filename, uint32_t archive_offset);
+extern LIBMPQ_API int32_t libmpq__archive_open(mpq_archive_s **mpq_archive, const char *mpq_filename, uint32_t archive_offset);
 extern LIBMPQ_API int32_t libmpq__archive_close(mpq_archive_s *mpq_archive);
 extern LIBMPQ_API int32_t libmpq__archive_compressed_size(mpq_archive_s *mpq_archive, off_t *compressed_size);
 extern LIBMPQ_API int32_t libmpq__archive_uncompressed_size(mpq_archive_s *mpq_archive, off_t *uncompressed_size);
