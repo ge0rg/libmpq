9ec68ee124bfc9463f1eb3ec425dcd9b2b90f744
forrestv <forrestv@6ba706d0-67d3-4ab1-bfe2-9ea6903dfcfc>
2009-03-27 05:39:02 +0000

    Added support for library finding on platforms besides Linux.
    git-svn-id: https://libmpq.org/svn/libmpq/trunk@285 6ba706d0-67d3-4ab1-bfe2-9ea6903dfcfc

--- bindings/python/mpq.py
+++ bindings/python/mpq.py
@@ -15,8 +15,9 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 import ctypes
+import ctypes.util
 
-libmpq = ctypes.CDLL("libmpq.so")
+libmpq = ctypes.CDLL(ctypes.util.find_library("mpq"))
 
 class Error(Exception):
     pass
