ge0rg/libmpq

commit 60c1e1a35dfca47b03768b00248166eb4849647f

modified API to reach a final state for the upcoming libmpq version
modified API to reach a final state for the upcoming libmpq version

bindings/python/mpq.py
@@ -50,14 +50,14 @@ libmpq.libmpq__version.restype = ctypes.c_char_p
 
 libmpq.libmpq__archive_open.errcheck = check_error
 libmpq.libmpq__archive_close.errcheck = check_error
-libmpq.libmpq__archive_packed_size.errcheck = check_error
-libmpq.libmpq__archive_unpacked_size.errcheck = check_error
+libmpq.libmpq__archive_size_packed.errcheck = check_error
+libmpq.libmpq__archive_size_unpacked.errcheck = check_error
 libmpq.libmpq__archive_offset.errcheck = check_error
 libmpq.libmpq__archive_version.errcheck = check_error
 libmpq.libmpq__archive_files.errcheck = check_error
 
-libmpq.libmpq__file_packed_size.errcheck = check_error
-libmpq.libmpq__file_unpacked_size.errcheck = check_error
+libmpq.libmpq__file_size_packed.errcheck = check_error
+libmpq.libmpq__file_size_unpacked.errcheck = check_error
 libmpq.libmpq__file_offset.errcheck = check_error
 libmpq.libmpq__file_blocks.errcheck = check_error
 libmpq.libmpq__file_encrypted.errcheck = check_error
@@ -68,7 +68,7 @@ libmpq.libmpq__file_read.errcheck = check_error
 
 libmpq.libmpq__block_open_offset.errcheck = check_error
 libmpq.libmpq__block_close_offset.errcheck = check_error
-libmpq.libmpq__block_unpacked_size.errcheck = check_error
+libmpq.libmpq__block_size_unpacked.errcheck = check_error
 libmpq.libmpq__block_read.errcheck = check_error
 
 __version__ = libmpq.libmpq__version()
@@ -112,7 +112,7 @@ class Reader(object):
     
     def _read_block(self, ctypes=ctypes, libmpq=libmpq):
         block_size = ctypes.c_uint64()
-        libmpq.libmpq__block_unpacked_size(self._file._archive._mpq,
+        libmpq.libmpq__block_size_unpacked(self._file._archive._mpq,
             self._file.number, self._cur_block, ctypes.byref(block_size))
         block_data = ctypes.create_string_buffer(block_size.value)
         libmpq.libmpq__block_read(self._file._archive._mpq,

FAQ
@@ -57,7 +57,7 @@ A: Of course :) The example below takes first parameter as mpq archive
            libmpq__archive_open(&mpq_archive, argv[1], -1);
 
            /* get size of first file (0) and malloc output buffer. */
-           libmpq__file_unpacked_size(mpq_archive, 0, &out_size);
+           libmpq__file_size_unpacked(mpq_archive, 0, &out_size);
            out_buf = malloc(out_size);
 
            /* read, decrypt and unpack file to output buffer. */

bindings/d/mpq.d
@@ -58,15 +58,15 @@ char *libmpq__version();
 /* libmpq__generic mpq archive information. */
 int libmpq__archive_open(mpq_archive_s **mpq_archive, char *mpq_filename, off_t archive_offset);
 int libmpq__archive_close(mpq_archive_s *mpq_archive);
-int libmpq__archive_packed_size(mpq_archive_s *mpq_archive, off_t *packed_size);
-int libmpq__archive_unpacked_size(mpq_archive_s *mpq_archive, off_t *unpacked_size);
+int libmpq__archive_size_packed(mpq_archive_s *mpq_archive, off_t *packed_size);
+int libmpq__archive_size_unpacked(mpq_archive_s *mpq_archive, off_t *unpacked_size);
 int libmpq__archive_offset(mpq_archive_s *mpq_archive, off_t *offset);
 int libmpq__archive_version(mpq_archive_s *mpq_archive, uint *version_);
 int libmpq__archive_files(mpq_archive_s *mpq_archive, uint *files);
 
 /* libmpq__generic file processing functions. */
-int libmpq__file_packed_size(mpq_archive_s *mpq_archive, uint file_number, off_t *packed_size);
-int libmpq__file_unpacked_size(mpq_archive_s *mpq_archive, uint file_number, off_t *unpacked_size);
+int libmpq__file_size_packed(mpq_archive_s *mpq_archive, uint file_number, off_t *packed_size);
+int libmpq__file_size_unpacked(mpq_archive_s *mpq_archive, uint file_number, off_t *unpacked_size);
 int libmpq__file_offset(mpq_archive_s *mpq_archive, uint file_number, off_t *offset);
 int libmpq__file_blocks(mpq_archive_s *mpq_archive, uint file_number, uint *blocks);
 int libmpq__file_encrypted(mpq_archive_s *mpq_archive, uint file_number, uint *encrypted);
@@ -78,7 +78,7 @@ int libmpq__file_read(mpq_archive_s *mpq_archive, uint file_number, ubyte *out_b
 /* libmpq__generic block processing functions. */
 int libmpq__block_open_offset(mpq_archive_s *mpq_archive, uint file_number);
 int libmpq__block_close_offset(mpq_archive_s *mpq_archive, uint file_number);
-int libmpq__block_unpacked_size(mpq_archive_s *mpq_archive, uint file_number, uint block_number, off_t *unpacked_size);
+int libmpq__block_size_unpacked(mpq_archive_s *mpq_archive, uint file_number, uint block_number, off_t *unpacked_size);
 int libmpq__block_read(mpq_archive_s *mpq_archive, uint file_number, uint block_number, ubyte *out_buf, off_t out_size, off_t *transferred);
 
 }

doc/man3/Makefile.am
@@ -11,21 +11,21 @@ man_MANS =				\
 	libmpq__archive_files.3		\
 	libmpq__archive_offset.3	\
 	libmpq__archive_open.3		\
-	libmpq__archive_packed_size.3	\
-	libmpq__archive_unpacked_size.3	\
+	libmpq__archive_size_packed.3	\
+	libmpq__archive_size_unpacked.3	\
 	libmpq__archive_version.3	\
 	libmpq__block_close_offset.3	\
 	libmpq__block_open_offset.3	\
 	libmpq__block_read.3		\
-	libmpq__block_unpacked_size.3	\
+	libmpq__block_size_unpacked.3	\
 	libmpq__file_blocks.3		\
 	libmpq__file_compressed.3	\
 	libmpq__file_encrypted.3	\
 	libmpq__file_imploded.3		\
 	libmpq__file_number.3		\
 	libmpq__file_offset.3		\
-	libmpq__file_packed_size.3	\
 	libmpq__file_read.3		\
-	libmpq__file_unpacked_size.3	\
+	libmpq__file_size_packed.3	\
+	libmpq__file_size_unpacked.3	\
 	libmpq__strerror.3		\
 	libmpq__version.3

doc/man3/libmpq.3
@@ -41,12 +41,12 @@ libmpq \- cross-platform C library for manipulating mpq archives.
 .BI "        mpq_archive_s  *" "mpq_archive"
 .BI ");"
 .sp
-.BI "int32_t libmpq__archive_packed_size("
+.BI "int32_t libmpq__archive_size_packed("
 .BI "        mpq_archive_s  *" "mpq_archive",
 .BI "        off_t          *" "packed_size"
 .BI ");"
 .sp
-.BI "int32_t libmpq__archive_unpacked_size("
+.BI "int32_t libmpq__archive_size_unpacked("
 .BI "        mpq_archive_s  *" "mpq_archive",
 .BI "        off_t          *" "unpacked_size"
 .BI ");"
@@ -66,13 +66,13 @@ libmpq \- cross-platform C library for manipulating mpq archives.
 .BI "        uint32_t       *" "files"
 .BI ");"
 .sp
-.BI "int32_t libmpq__file_packed_size("
+.BI "int32_t libmpq__file_size_packed("
 .BI "        mpq_archive_s  *" "mpq_archive",
 .BI "        uint32_t        " "file_number",
 .BI "        off_t          *" "packed_size"
 .BI ");"
 .sp
-.BI "int32_t libmpq__file_unpacked_size("
+.BI "int32_t libmpq__file_size_unpacked("
 .BI "        mpq_archive_s  *" "mpq_archive",
 .BI "        uint32_t        " "file_number",
 .BI "        off_t          *" "unpacked_size"
@@ -132,14 +132,14 @@ libmpq \- cross-platform C library for manipulating mpq archives.
 .BI "        uint32_t        " "file_number"
 .BI ");"
 .sp
-.BI "int32_t libmpq__block_packed_size("
+.BI "int32_t libmpq__block_size_packed("
 .BI "        mpq_archive_s  *" "mpq_archive",
 .BI "        uint32_t        " "file_number",
 .BI "        uint32_t        " "block_number",
 .BI "        off_t          *" "packed_size"
 .BI ");"
 .sp
-.BI "int32_t libmpq__block_unpacked_size("
+.BI "int32_t libmpq__block_size_unpacked("
 .BI "        mpq_archive_s  *" "mpq_archive",
 .BI "        uint32_t        " "file_number",
 .BI "        uint32_t        " "block_number",
@@ -177,13 +177,13 @@ The \fIlibmpq\fP library supports decrypting, decompressing, exploding and vario
 .BR libmpq__strerror (3),
 .BR libmpq__archive_open (3),
 .BR libmpq__archive_close (3),
-.BR libmpq__archive_packed_size (3),
-.BR libmpq__archive_unpacked_size (3),
+.BR libmpq__archive_size_packed (3),
+.BR libmpq__archive_size_unpacked (3),
 .BR libmpq__archive_offset (3),
 .BR libmpq__archive_version (3),
 .BR libmpq__archive_files (3),
-.BR libmpq__file_packed_size (3),
-.BR libmpq__file_unpacked_size (3),
+.BR libmpq__file_size_packed (3),
+.BR libmpq__file_size_unpacked (3),
 .BR libmpq__file_offset (3),
 .BR libmpq__file_blocks (3),
 .BR libmpq__file_encrypted (3),
@@ -193,8 +193,8 @@ The \fIlibmpq\fP library supports decrypting, decompressing, exploding and vario
 .BR libmpq__file_read (3),
 .BR libmpq__block_open_offset (3),
 .BR libmpq__block_close_offset (3),
-.BR libmpq__block_packed_size (3),
-.BR libmpq__block_unpacked_size (3),
+.BR libmpq__block_size_packed (3),
+.BR libmpq__block_size_unpacked (3),
 .BR libmpq__block_offset (3),
 .BR libmpq__block_seed (3),
 .BR libmpq__block_read (3)

doc/man3/libmpq__archive_close.3
@@ -43,8 +43,8 @@ On success, a zero is returned and on error one of the following constants.
 The given file could not be closed.
 .SH SEE ALSO
 .BR libmpq__archive_open (3),
-.BR libmpq__archive_packed_size (3),
-.BR libmpq__archive_unpacked_size (3),
+.BR libmpq__archive_size_packed (3),
+.BR libmpq__archive_size_unpacked (3),
 .BR libmpq__archive_offset (3),
 .BR libmpq__archive_version (3),
 .BR libmpq__archive_files (3)

doc/man3/libmpq__archive_open.3
@@ -57,8 +57,8 @@ The given file is no valid mpq archive.
 Reading in archive failed.
 .SH SEE ALSO
 .BR libmpq__archive_close (3),
-.BR libmpq__archive_packed_size (3),
-.BR libmpq__archive_unpacked_size (3),
+.BR libmpq__archive_size_packed (3),
+.BR libmpq__archive_size_unpacked (3),
 .BR libmpq__archive_offset (3),
 .BR libmpq__archive_version (3),
 .BR libmpq__archive_files (3)

doc/man3/libmpq__archive_size_packed.3
@@ -19,7 +19,7 @@
 .\" License along with this manual; if not, write to the Free
 .\" Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111,
 .\" USA.
-.TH libmpq 3 2008-05-15 "The MoPaQ archive library"
+.TH libmpq 3 2008-04-29 "The MoPaQ archive library"
 .SH NAME
 libmpq \- cross-platform C library for manipulating mpq archives.
 .SH SYNOPSIS
@@ -27,25 +27,21 @@ libmpq \- cross-platform C library for manipulating mpq archives.
 .B
 #include <mpq.h>
 .sp
-.BI "int32_t libmpq__file_unpacked_size("
+.BI "int32_t libmpq__archive_size_packed("
 .BI "        mpq_archive_s  *" "mpq_archive",
-.BI "        uint32_t        " "file_number",
-.BI "        off_t          *" "unpacked_size"
+.BI "        off_t          *" "packed_size"
 .BI ");"
 .fi
 .SH DESCRIPTION
 .PP
-Call \fBlibmpq__file_unpacked_size\fP() to get the unpacked size of a given file in the archive. It will return a valid size for compressed and imploded files as well as stored only.
+Call \fBlibmpq__archive_size_packed\fP() to get the packed size of all files in the archive. It will count compressed and imploded files as well as stored only.
 .LP
-The \fBlibmpq__file_unpacked_size\fP() function takes as first argument the archive structure \fImpq_archive\fP which have to be allocated first and opened by \fBlibmpq__archive_open\fP(). The second argument \fIfile_number\fP is the number of file and the third argument is a reference to the uncompressed, exploded or stored size \fIunpacked_size\fP of file.
+The \fBlibmpq__archive_size_packed\fP() function takes as first argument the archive structure \fImpq_archive\fP which have to be allocated first and opened by \fBlibmpq__archive_open\fP(). The second argument is a reference to the compressed, imploded or stored size \fIpacked_size\fP of file.
 .SH RETURN VALUE
-On success, a zero is returned and on error one of the following constants.
-.TP
-.B LIBMPQ_ERROR_EXIST
-File does not exist in archive.
+On success, a zero is returned.
 .SH SEE ALSO
-.BR libmpq__archive_unpacked_size (3),
-.BR libmpq__block_unpacked_size (3)
+.BR libmpq__file_size_packed (3),
+.BR libmpq__block_size_packed (3)
 .SH AUTHOR
 Check documentation.
 .TP

commit addcb3eaabb1c1ca50db3b20744c30a81ad8e399

fixed python bindings, broken by commit 60c1e1a3 "modified API to reach a final state for the upcoming libmpq version"

bindings/python/mpq.py
@@ -95,7 +95,7 @@ class Reader(object):
         elif whence == os.SEEK_CUR:
             offset += self._pos
         elif whence == os.SEEK_END:
-            offset += self._file.unpacked_size
+            offset += self._file.size_unpacked
         else:
             raise ValueError, "invalid whence"
         
@@ -176,8 +176,8 @@ class File(object):
         self.number = number
         
         for name, atype in [
-            ("packed_size", ctypes.c_uint64),
-            ("unpacked_size", ctypes.c_uint64),
+            ("size_packed", ctypes.c_uint64),
+            ("size_unpacked", ctypes.c_uint64),
             ("offset", ctypes.c_uint64),
             ("blocks", ctypes.c_uint32),
             ("encrypted", ctypes.c_uint32),
@@ -189,8 +189,11 @@ class File(object):
             func(self._archive._mpq, self.number, ctypes.byref(data))
             setattr(self, name, data.value)
     
+    packed_size = property(lambda self: self.size_packed)
+    unpacked_size = property(lambda self: self.size_unpacked)
+    
     def __str__(self, ctypes=ctypes, libmpq=libmpq):
-        data = ctypes.create_string_buffer(self.unpacked_size)
+        data = ctypes.create_string_buffer(self.size_unpacked)
         libmpq.libmpq__file_read(self._archive._mpq, self.number,
             data, ctypes.c_uint64(len(data)), None)
         return data.raw
@@ -221,8 +224,8 @@ class Archive(object):
         self._opened = True
         
         for field_name, field_type in [
-            ("packed_size", ctypes.c_uint64),
-            ("unpacked_size", ctypes.c_uint64),
+            ("size_packed", ctypes.c_uint64),
+            ("size_unpacked", ctypes.c_uint64),
             ("offset", ctypes.c_uint64),
             ("version", ctypes.c_uint32),
             ("files", ctypes.c_uint32),
@@ -231,6 +234,9 @@ class Archive(object):
             data = field_type()
             func(self._mpq, ctypes.byref(data))
             setattr(self, field_name, data.value)
+    
+    packed_size = property(lambda self: self.size_packed)
+    unpacked_size = property(lambda self: self.size_unpacked)
     
     def __del__(self, libmpq=libmpq):
         if getattr(self, "_opened", False):
@@ -308,7 +314,7 @@ if __name__ == "__main__":
         d = "".join(d)
         
         assert a == b == c == d, map(hash, [a,b,c,d])
-        assert len(a) == file.unpacked_size
+        assert len(a) == file.size_unpacked
         
         repr(iter(file))
         